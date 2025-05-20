0a4807728e882cca91ab5ffd89dd03f395eee755
iceymoss <114280774+iceymoss@users.noreply.github.com>
2025-05-20 10:19:30 +0800

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,12 @@
 // src/lib.rs
 //使用 lib.rs 作为模块聚合点
+// 需要对外使用的model在这里导出
 pub mod commands;  // 导出 commands 模块
+pub mod config;  // 导出 config 模块
+pub mod entities;
+pub mod handlers;
+pub mod models;
+pub mod routes;
+pub mod services;
+
+pub mod tasks;
