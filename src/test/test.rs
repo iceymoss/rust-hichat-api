a99ded2773d78ca894313307382cfe0dbb51cea7
iceymoss <114280774+iceymoss@users.noreply.github.com>
2025-05-20 10:22:45 +0800

    Update test.rs

--- src/test/test.rs
+++ src/test/test.rs
@@ -3,23 +3,24 @@
 // redis = { version = "0.23", features = ["tokio-comp"] }
 // tokio = { version = "1.0", features = ["full"] }
 
-use redis::AsyncCommands;
-use redis::Client;
+
 use std::error::Error;
+use rust_hichat_api::config::redis::{init_rdb, get_conn};
+use deadpool_redis::redis::AsyncCommands;
+
 
 // tokio 是运行时
 #[tokio::main] // 启用 Tokio 异步运行时
 async fn main() -> Result<(), Box<dyn Error>> {
-    // 创建 Redis 客户端
-    let client = Client::open("redis://localhost:6379")?;
-
-    // 获取异步连接
-    let mut conn = client.get_async_connection().await?;
-
+    init_rdb().await?;
+    
+    // 正确方式：用 ? 解包获取 Connection
+    let mut conn = get_conn().await?;
+    
     // 执行 SET 命令
     // conn.set("my_key", "hello_redis").await?;
     // 显式告诉编译器返回值类型是 `()`
-    let _: () = conn.set("my_key", "hello_redis").await?;
+    let _: () = conn.set("my_key", "hello").await?;
     println!("Key set successfully.");
 
     // 执行 GET 命令
